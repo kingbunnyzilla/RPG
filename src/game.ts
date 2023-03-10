e9921ced1f3d79d3db042ad837b7163cff39cf6b
Teagen Bell <teagen.bell@gmail.com>
2023-03-10 16:26:30 -0500

    Resized Main Sprite

--- src/game.ts
+++ src/game.ts
@@ -1,29 +1,54 @@
 import Phaser from "phaser";
-import "./assets/player.jpg";
+import "./assets/Walk_Down.png";
+import "./assets/dude.png";
 
-let player:Phaser.Physics.Arcade.Sprite;
+let player: Phaser.Physics.Arcade.Sprite;
+let player2: Phaser.Physics.Arcade.Sprite;
+
 new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
     scene: {
-        preload(){
-            this.load.spritesheet('player',
-                'assets/player.jpg',
-                { frameWidth: 36, frameHeight: 36 }
+        preload() {
+            this.load.spritesheet('Walk_Down',
+                'assets/Walk_Down.png',
+                { frameWidth: 32, frameHeight: 32 }
+            );
+            this.load.spritesheet('dude',
+                'assets/dude.png',
+                { frameWidth: 32, frameHeight: 48 }
             );
         },
-        create(){
-            player = this.physics.add.sprite(100, 450, 'player');
+
+        create() {
+            player = this.physics.add.sprite(100, 450, 'Walk_Down');
             this.anims.create({
-                key: "walk down",
-                frames: this.anims.generateFrameNumbers('player',{start:0, end: 6}),
+                key: "walkdown",
+                frames: this.anims.generateFrameNumbers('Walk_Down', { start: 0, end: 7 }),
                 frameRate: 10,
                 repeat: -1
             });
+            player2 = this.physics.add.sprite(200, 450, 'dude');
+            this.anims.create({
+                key: 'left',
+                frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 8 }),
+                frameRate: 10,
+                repeat: -1
+            });
+
         },
-        update(){
-            player.anims.play("walk down", true);
+
+        update() {
+            player.anims.play("walkdown", true);
+            player2.anims.play("left", true);
 
         }
     }
